6640732802af11296e23c9a7ac1e8ae85691d5bb
Derrick Oswald <derrick.oswald@gmail.com>
2018-03-12 17:02:05 +0100

    fix Spark 2.2.1

--- spark-env.sh
+++ spark-env.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export HADOOP_HOME=/usr/local/hadoop-2.7.3
+export HADOOP_HOME=/usr/local/hadoop-2.7.4
 export HADOOP_LIBEXEC_DIR=$HADOOP_HOME/libexec
 export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop
 export SPARK_DIST_CLASSPATH=$(hadoop classpath)
